6a0613b5cb683f18d29b997f11f802504db738c1
Djessica <djessi92@gmail.com>
2020-01-21 15:21:37 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/djessicaeickstaedt/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/djessicaeickstaedt/cursomc/resource/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.djessicaeickstaedt.cursomc.resource;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.djessicaeickstaedt.cursomc.domain.Categoria;
 import com.djessicaeickstaedt.cursomc.services.CategoriaService;
@@ -18,11 +22,23 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 
-	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
-		
+
 		Categoria obj = service.find(id);
-		
+
 		return ResponseEntity.ok(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	/*
+	 * para que o obj seja construido com os dados json mandados na querisição post usa se o requestBody
+	 * ou seja ele converte o json para o objeto categoria
+	 */
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		/* fromCurrentRequest vai pegar a url que está antes do id da categoria */
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
