152d95db1432cf972fdc2f353cd311cdd9a649fe
Djessica <djessi92@gmail.com>
2020-01-21 22:30:32 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/djessicaeickstaedt/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/djessicaeickstaedt/cursomc/resource/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.djessicaeickstaedt.cursomc.resource;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.djessicaeickstaedt.cursomc.domain.Categoria;
+import com.djessicaeickstaedt.cursomc.dto.CategoriaDTO;
 import com.djessicaeickstaedt.cursomc.services.CategoriaService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -32,8 +35,8 @@ public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.POST)
 	/*
-	 * para que o obj seja construido com os dados json mandados na querisição post usa se o requestBody
-	 * ou seja ele converte o json para o objeto categoria
+	 * para que o obj seja construido com os dados json mandados na querisição post
+	 * usa se o requestBody ou seja ele converte o json para o objeto categoria
 	 */
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
 		obj = service.insert(obj);
@@ -41,18 +44,25 @@ public class CategoriaResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
 	}
 }
--- src/main/java/com/djessicaeickstaedt/cursomc/services/CategoriaService.java
+++ src/main/java/com/djessicaeickstaedt/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.djessicaeickstaedt.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,29 +12,28 @@ import com.djessicaeickstaedt.cursomc.repositories.CategoriaRepository;
 import com.djessicaeickstaedt.cursomc.services.exceptions.DataIntegrityException;
 import com.djessicaeickstaedt.cursomc.services.exceptions.ObjectNotFoundException;
 
-
-
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) throws ObjectNotFoundException {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -41,7 +41,10 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir categoria que tem produtos");
 		}
-		
+
 	}
 
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 }
